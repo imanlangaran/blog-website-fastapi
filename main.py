63da728528013e2d52a916bde3d86ec181eaeaf4
Iman Langaran <imanlangaran@gmail.com>
2023-08-08 08:45:04 +0330

    add static files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from core.config import setting
 from db.session import engine  # , Base
@@ -16,18 +17,23 @@ def include_routes(app: FastAPI):
     app.include_router(router=router)
 
 
+def config_staticfiles(app: FastAPI):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_app():
     app = FastAPI(title=setting.APP_TITLE, version=setting.APP_VERSION)
     create_tables()
     include_routes(app)
+    config_staticfiles(app)
     return app
+
 
 templates = Jinja2Templates(directory="templates")
 
 app = start_app()
 
 
-
 @app.get("/")
-def home(request:Request):
-    return templates.TemplateResponse("blog/home.html", {"request":request})
+def home(request: Request):
+    return templates.TemplateResponse("blog/home.html", {"request": request})
